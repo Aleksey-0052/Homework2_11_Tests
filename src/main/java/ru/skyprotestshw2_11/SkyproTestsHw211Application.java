4352616b297fb6bd83590ad547299490fe08bc46
alex <alexsemen666653@gmail.com>
2023-11-04 20:52:02 +0300

    Initial commit

---
+++ src/main/java/ru/skyprotestshw2_11/SkyproTestsHw211Application.java
@@ -0,0 +1,13 @@
+package ru.skyprotestshw2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTestsHw211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproTestsHw211Application.class, args);
+	}
+
+}
