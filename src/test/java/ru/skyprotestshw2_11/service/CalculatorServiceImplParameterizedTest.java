d38d270104f048b3c1a5d440efd42ee875948804
alex <alexsemen666653@gmail.com>
2023-11-06 14:35:36 +0300

    Выполнено ДЗ c комментариями

--- src/test/java/ru/skyprotestshw2_11/service/CalculatorServiceImplParameterizedTest.java
+++ src/test/java/ru/skyprotestshw2_11/service/CalculatorServiceImplParameterizedTest.java
@@ -3,6 +3,7 @@ package ru.skyprotestshw2_11.service;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -60,10 +61,24 @@ class CalculatorServiceImplParameterizedTest {
 
     @ParameterizedTest
     @MethodSource("sumCases")
-    void sumParams(int num1, int num2, int expected) {
+    void sumParams1(int num1, int num2, int expected) {
         int result = calculatorService.sum(num1, num2);
         assertEquals(expected, result);
     }
+
+    // Можно использовать аннотацию @CsvSource // В этом случае создавать стримы с аргументами не нужно
+    // Аргументы указываются в кавычках и фигурных скобках
+
+    @ParameterizedTest
+    @CsvSource({"1,1,2", "10,10,20", "-5, 10, 5"})
+    void sumParams2(int num1, int num2, int expected) {
+        int result = calculatorService.sum(num1, num2);
+        assertEquals(expected, result);
+    }
+    // Если параметром является строка, то используют аннотацию @NullAndEmptySource("")
+    
+    // Если входной параметр только один, то используют аннотацию @ValueSource(ints = {1,1,2})
+    // переменная int num будет поочередно принимать значения из фигурных скобок
 
     @ParameterizedTest
     @MethodSource("subtractCases")
